cc3978bca2c676c97f0b25ba7980b50a7bfdb76f
Rafael Mendes <rafarrmends@gmail.com>
2018-08-30 08:10:13 -0300

    Versão anterior

--- tcc/index.php
+++ tcc/index.php
@@ -79,7 +79,7 @@
           }
           else if($link == 'oferta'){
             include('restrito/operacoes/oferta.php');
-            echo "<script src='js/ajax/ajax_oferta.js'></script>";
+            echo "<script src='js/ajax/ajax_oferta.js'</script>";
           }
       	}
       ?>
@@ -94,13 +94,5 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
-    <!-- <?php 
-      // if(isset($_GET['pag']))
-      //    $link = $_GET['pag'];
-          
-      //     if($link == 'cadcurso'){
-      //       include('restrito/cads/cad_curso.php');
-      //     }
-    ?> -->
   </body>
 </html>
