21756ced247d1356e18e1a8ae08d0230d697cf89
Rafael Mendes <rafarrmends@gmail.com>
2018-09-11 20:52:38 -0300

    Atualizando script de horário

--- tcc/restrito/acoes/script_horario.php
+++ tcc/restrito/acoes/script_horario.php
@@ -7,20 +7,29 @@
 			INNER JOIN oferta as o ON h.ofe_cod = o.ofe_cod
 			INNER JOIN serie_has_turma as st ON o.ser_cod = st.ser_cod
 			INNER JOIN turma as t ON st.tur_cod = t.tur_cod
-			WHERE ofe_cod = " . $turma;
-	$script = mysqli_query($con, $sql);
+			INNER JOIN professor_has_disciplina as pd ON o.pd_cod = pd.pd_cod
+			INNER JOIN disciplina as d ON pd.dis_cod = d.dis_cod
+			INNER JOIN professor as p ON pd.pro_cod = p.pro_cod
+			WHERE h.ofe_id = " . ($turma+3);
+	$script = mysqli_query($con, $sql) or die('Falha ao buscar horário de turma');
 ?>
 
 <table class="table table-hover">
-	<th>Domingo</th>
-	<th>Segunda</th>
-	<th>Terça</th>
-	<th>Quarta</th>
-	<th>Quinta</th>
-	<th>Sexta</th>
-	<th>Sábado</th>
+	<tr>
+		<th>Domingo</th>
+		<th>Segunda</th>
+		<th>Terça</th>
+		<th>Quarta</th>
+		<th>Quinta</th>
+		<th>Sexta</th>
+		<th>Sábado</th>
+	</tr>
+	<?php
+		while($horario = mysqli_fetch_array($script)){
+			echo "<tr>
+					<td>" . $horario['dis_nome'] ."</td>
+				</tr>";
+		}
+	?>
 </table>
 
-<?php
-
-?>
