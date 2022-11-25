00c1478b3e8f40111d809d09b4db378cc8292b8e
Rafael Mendes <rafarrmends@gmail.com>
2022-11-25 16:17:11 -0300

    Implementando PDO

--- src/connection/conecta.php
+++ src/connection/conecta.php
@@ -6,8 +6,12 @@
 	$db = 'db_pdt';
 
 	// Conexão com o banco de dados: Instrução SQL
-	$con=mysqli_connect($server, $user, $pswd, $db);
-	if(mysqli_connect_errno($con)){
-		echo 'Falha na conexão com o banco de dados';
-	}
+	try{
+    $conn = new PDO("mysql:host=$server;dbname=$db,", $user, $pswd);
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conectado ao banco de dados com sucesso";
+  } catch(PDOException $e){
+    echo 'Falha na conexão com o banco de dados:' . $e->getMessage();
+  }
 ?>
