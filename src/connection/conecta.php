a1021f2641f3667f542237ec6ea9e020d85fd4eb
Rafael Mendes <rafarrmends@gmail.com>
2022-11-25 17:38:40 -0300

    Correção do Tab Size

--- src/connection/conecta.php
+++ src/connection/conecta.php
@@ -7,11 +7,11 @@
 
 	// Conexão com o banco de dados: Instrução SQL
 	try{
-    $conn = new PDO("mysql:host=$server;dbname=$db,", $user, $pswd);
-    
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conectado ao banco de dados com sucesso";
-  } catch(PDOException $e){
-    echo 'Falha na conexão com o banco de dados:' . $e->getMessage();
-  }
+        $conn = new PDO("mysql:host=$server;dbname=$db,", $user, $pswd);
+        
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conectado ao banco de dados com sucesso";
+    } catch(PDOException $e){
+        echo 'Falha na conexão com o banco de dados:' . $e->getMessage();
+    }
 ?>
